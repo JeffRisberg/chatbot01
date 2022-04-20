a81ec94f486bf97c558bda550cb0233d67e7bc83
JeffRisberg <j.s.risberg@gmail.com>
2022-04-20 04:53:26 -0700

    added a logging statement for message input

--- src/bot/MessageParser.js
+++ src/bot/MessageParser.js
@@ -4,10 +4,11 @@ class MessageParser {
   }
 
   parse(message) {
+    console.log(message);
     const lowerCaseMessage = message.toLowerCase();
 
     if (lowerCaseMessage.includes("hello")) {
-      this.actionProvider.greet();
+      this.actionProvider.handleHello();
     }
 
     if (lowerCaseMessage.includes("javascript")) {
