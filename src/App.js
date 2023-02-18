b3f54717c72fa8416d5ce73960767f1482461dcb
JeffRisberg <j.s.risberg@gmail.com>
2023-02-17 19:24:48 -0800

    removed warning

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Chatbot from "react-chatbot-kit";
 import 'react-chatbot-kit/build/main.css'
@@ -11,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Chatbot 
+        <Chatbot
          config={config}
          messageParser={MessageParser}
          actionProvider={ActionProvider}
